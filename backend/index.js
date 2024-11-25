22c3bb56176ea3568d9cdb75195f3307d085f897
Peter Pardo <peterpardo123@gmail.com>
2024-11-25 20:30:36 +0800

    first commit

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const cookieParse = require("cookie-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", require("./routes/auth.route"));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port=${PORT}`);
+});
